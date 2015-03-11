cb308b6d662bd703189005e4b353a65cb03d828b
Jonathan Reem <jonathan.reem@gmail.com>
2015-03-11 15:16:08 -0700

    (refactor) Refactor the Chunks and Consumer traits.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#![feature(io, core)]
 #![cfg_attr(test, deny(warnings))]
 //#![deny(missing_docs)]
 
@@ -7,42 +8,50 @@
 //!
 
 extern crate syncbox;
-extern crate rcmut;
 extern crate bytes;
 extern crate mio;
 
 pub use syncbox::util::async::Future;
 use bytes::traits::*;
+use bytes::{SliceBuf, MutSliceBuf};
+use std::io;
 
 pub trait Chunks: Sized + Send {
-    type Error: Send;
     type Reader: ChunkReader<Self>;
 
-    fn chunk(self) -> Future<Self::Reader, Self::Error>;
+    fn chunk(self) -> Future<Self::Reader, io::Error>;
 
-    fn pipe<W: Consumer<Error=Self::Error>>(self, write: W) -> Future<(), Self::Error> {
+    fn pipe<W: Consumer>(self, write: W) -> Future<(Self, W), io::Error> {
         pipe::pipe(self, write)
     }
 }
 
 pub trait ChunkReader<R: Chunks>: Sized + Send {
-    fn read<M: MutBuf>(self, &mut M) -> Result<(usize, Option<R>), R::Error>;
+    fn read<M: MutBuf>(self, &mut M) -> io::Result<(usize, R)>;
+    fn read_slice(self, buf: &mut [u8]) -> io::Result<(usize, R)> {
+        self.read(&mut MutSliceBuf::wrap(buf))
+    }
 }
 
 pub trait Consumer: Sized + Send {
-    type Error: Send;
     type Writer: ChunkConsumer<Self>;
 
-    fn chunk(self) -> Future<Self::Writer, Self::Error>;
+    fn chunk(self) -> Future<Self::Writer, io::Error>;
 
-    fn consume<R: Chunks<Error=Self::Error>>(self, read: R) -> Future<(), R::Error> {
+    fn consume<R: Chunks>(self, read: R) -> Future<(R, Self), io::Error> {
         read.pipe(self)
     }
 }
 
 pub trait ChunkConsumer<W: Consumer>: Sized + Send {
-    fn write<B: Buf>(self, &mut B) -> Result<(usize, Option<W>), W::Error>;
+    fn write<B: Buf>(self, &mut B) -> io::Result<(usize, W)>;
+
+    fn write_slice(self, buf: &[u8]) -> io::Result<(usize, W)> {
+        self.write(&mut SliceBuf::wrap(buf))
+    }
 }
 
 mod pipe;
+pub mod util;
+pub mod impls;
 
